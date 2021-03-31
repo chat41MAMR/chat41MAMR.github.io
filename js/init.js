15299d1d46aab126815472a1b2ba7bef9b2a5f2c
chat41MAMR <81317159+chat41MAMR@users.noreply.github.com>
2021-03-30 21:13:24 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,10 @@
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDq3Q6QW7ep3oVqm2eF8V0hFHjVTNnhKOc",
-    authDomain: "examenfinal-b6e16.firebaseapp.com",
-    projectId: "examenfinal-b6e16",
-    storageBucket: "examenfinal-b6e16.appspot.com",
-    messagingSenderId: "459376802378",
-    appId: "1:459376802378:web:ae903898b637cb200444b3",
-    measurementId: "G-SMYY1VFMD6"
+    apiKey: "AIzaSyAIn07Dom0gwOZsldLoYbQKMKXtLfxwZpk",
+    authDomain: "examfinl.firebaseapp.com",
+    projectId: "examfinl",
+    storageBucket: "examfinl.appspot.com",
+    messagingSenderId: "699029411374",
+    appId: "1:699029411374:web:4f458f4aa2f1732e2e71bf",
+    measurementId: "G-GW19KXJXNG"
 });
